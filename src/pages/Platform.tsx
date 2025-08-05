902837e4503e0f9d91be92b70e750176051fb6fa
steeze19 <steeze266@gmail.com>
2025-08-05 20:52:19 +0100

    Update Platform.tsx

--- src/pages/Platform.tsx
+++ src/pages/Platform.tsx
@@ -9,11 +9,11 @@ const Platform = () => {
   const navigate = useNavigate();
   
   const handleJoinTelegram = () => {
-    window.open("https://t.me/officialbluepay2025", "_blank");
+    window.open("https://t.me/+Bit3dq-VJA4xMDNk", "_blank");
   };
   
   const handleJoinWhatsapp = () => {
-    window.open("https://chat.whatsapp.com/LUS3IRRaGrR3BueOPCObgb", "_blank");
+    window.open("https://chat.whatsapp.com/JgHl7v7Umql53lGV3NdTnA?mode=ac_t", "_blank");
   };
 
   return (
